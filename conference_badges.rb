c14170099d81452ae94a33883583d2dff232d41e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-26 19:57:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creater(luminary_array)
-  message_array = luminary_array.collect {
+  return luminary_array.collect {
     |name| "Hello, my name is #{name}."
   }
-
-  return message_array
 end
